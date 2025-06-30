2241c8b0fe9675c0e7e13b52ba227f2f97eb1b8e
Aniket Kedari <140852884+Aniket000k@users.noreply.github.com>
2025-06-30 11:44:29 +0530

    feat: add sign-in and sign-up pages with Clerk authentication

---
+++ components/header.jsx
@@ -0,0 +1,29 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <div className="fixed top-0">
+<nav>
+ <Link href="/">
+ <Image></Image>
+ </Link>
+</nav>
+
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton>
+          <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
+};
+
+export default Header;
