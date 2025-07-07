83fad92df18d970be66a64e3aa4b1724704ab7aa
Aniket Kedari <140852884+Aniket000k@users.noreply.github.com>
2025-07-07 09:54:26 +0530

    landing Page completed

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,31 +1,50 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Button } from "./ui/button";
 import Image from "next/image";
 
 const HeroSecrion = () => {
+    const imageRef = useRef();
+
+     useEffect(() => {
+    const imageElement = imageRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
   return (
     <div className="pb-20 px-4 ">
       <div className="container mx-auto p-6 pt-20 text-center">
-        <h1 className="text-5xl md:text-8xl lg:text-[105px]  mb-6">
+        <h1 className="text-5xl md:text-8xl lg:text-[105px] gradient-title  mb-6">
           Your Personal Finance Assistant <br /> Powered by AI
         </h1>
-        <p>
+        <p className="text-lg md:text-2xl lg:text-3xl text-gray-700 mb-8 max-w-2xl mx-auto">
           BudgetIQ helps you track your spending, plan your savings, and stay in
-          control of your money — all with the power of AI. Whether you're
-          managing daily expenses or setting long-term goals, our smart tools
-          make finance easy and stress-free.
+          control of your money all with the power of AI. 
         </p>
-        <div>
+        <div >
           <Link href="/dashboard">
             <Button size={"lg"} className="px-8">
               Get Started
             </Button>
           </Link>
         </div>
-        <div>
-          <div>
+        <div className="hero-image-wrapper">
+          <div className="hero-image" ref={imageRef}>
             <Image
               src={"/banner.jpeg"}
               width={1280}
